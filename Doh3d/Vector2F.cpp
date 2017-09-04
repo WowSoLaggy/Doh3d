8a1c1299f7f5054ddb4a29511c66295224a94b40
Anton Egorov <dohxehapo@gmail.com>
2017-09-04 22:21:15 +0300

    (+) Added Vector2F

---
+++ Doh3d/Vector2F.cpp
@@ -0,0 +1,49 @@
+#include "PrecompiledHeader.h"
+#include "Vector2F.h"
+
+
+namespace Doh3d
+{
+
+  Vector2F::Vector2F()
+    : Vector2F(0, 0)
+  {
+  }
+
+  Vector2F::Vector2F(int i_x, int i_y)
+    : x((float)i_x), y((float)i_y)
+  {
+  }
+
+  Vector2F::Vector2F(float i_x, float i_y)
+    : x(i_x), y(i_y)
+  {
+  }
+
+
+  Vector2F Vector2F::operator+(const Vector2F& i_other) const
+  {
+    return{ x + i_other.x, y + i_other.y };
+  }
+
+  Vector2F Vector2F::operator+=(const Vector2F& i_other)
+  {
+    x += i_other.x;
+    y += i_other.y;
+    return *this;
+  }
+
+  Vector2F Vector2F::operator-(const Vector2F& i_other) const
+  {
+    return{ x - i_other.x, y - i_other.y };
+  }
+
+  Vector2F Vector2F::operator-=(const Vector2F& i_other)
+  {
+    x -= i_other.x;
+    y -= i_other.y;
+    return *this;
+  }
+
+
+} // ns Doh3d
---
+++ Doh3d/Vector2F.h
@@ -0,0 +1,33 @@
+#pragma once
+
+
+namespace Doh3d
+{
+
+  class Vector2F
+  {
+  public:
+
+    Vector2F();
+    Vector2F(int i_x, int i_y);
+    Vector2F(float i_x, float i_y);
+
+    float x;
+    float y;
+
+
+    // Operators
+
+    Vector2F operator+(const Vector2F& i_other) const;
+    Vector2F operator+=(const Vector2F& i_other);
+    Vector2F operator-(const Vector2F& i_other) const;
+    Vector2F operator-=(const Vector2F& i_other);
+
+  public:
+
+    static Vector2F zero() { return{ 0, 0 }; }
+    static Vector2F identity() { return{ 1, 1 }; }
+
+  };
+
+} // ns Doh3d

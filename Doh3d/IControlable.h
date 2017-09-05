b46deda711034c631772bcee18c4855fe0620bcd
Anton Egorov <dohxehapo@gmail.com>
2017-09-05 13:43:40 +0300

    (+) Added actions to the controllable object

--- Doh3d/IControlable.h
+++ Doh3d/IControlable.h
@@ -6,6 +6,18 @@ namespace Doh3d
 
   class IControlable
   {
+  public:
+
+    virtual void goUpBegin() { }
+    virtual void goRightBegin() { }
+    virtual void goDownBegin() { }
+    virtual void goLeftBegin() { }
+
+    virtual void goUpEnd() { }
+    virtual void goRightEnd() { }
+    virtual void goDownEnd() { }
+    virtual void goLeftEnd() { }
+
   };
 
 } // ns Doh3d

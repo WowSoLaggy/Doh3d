b956b23daf0d4e838200ad2fbb79b595ab655515
Anton Egorov <dohxehapo@gmail.com>
2017-09-05 13:44:00 +0300

    (*) Controller now sends actions

--- Doh3d/Controller.cpp
+++ Doh3d/Controller.cpp
@@ -7,8 +7,9 @@
 namespace Doh3d
 {
 
-  Controller::Controller()
-    : d_bindedObject(nullptr)
+  Controller::Controller(ControllerId i_id)
+    : d_id(i_id)
+    , d_bindedObject(nullptr)
   {
   }
 
@@ -30,6 +31,41 @@ namespace Doh3d
   {
     if (d_bindedObject)
       d_bindedObject = nullptr;
+  }
+
+
+  bool Controller::onKeyPressed(Key i_key)
+  {
+    if (!d_bindedObject)
+      return true;
+
+    return true;
+  }
+
+  bool Controller::onKeyDown(Key i_key)
+  {
+    if (!d_bindedObject)
+      return true;
+
+    if (i_key == DIK_W) d_bindedObject->goUpBegin();
+    else if (i_key == DIK_D) d_bindedObject->goRightBegin();
+    else if (i_key == DIK_S) d_bindedObject->goDownBegin();
+    else if (i_key == DIK_A) d_bindedObject->goLeftBegin();
+
+    return true;
+  }
+
+  bool Controller::onKeyUp(Key i_key)
+  {
+    if (!d_bindedObject)
+      return true;
+
+    if (i_key == DIK_W) d_bindedObject->goUpEnd();
+    else if (i_key == DIK_D) d_bindedObject->goRightEnd();
+    else if (i_key == DIK_S) d_bindedObject->goDownEnd();
+    else if (i_key == DIK_A) d_bindedObject->goLeftEnd();
+
+    return true;
   }
 
 } // ns Doh3d
--- Doh3d/Controller.h
+++ Doh3d/Controller.h
@@ -1,21 +1,34 @@
 #pragma once
 
+#include "IInputHandler.h"
+
 
 namespace Doh3d
 {
 
   class IControlable;
+  using ControllerId = int;
 
-  class Controller
+
+  class Controller : public IInputHandler
   {
   public:
 
-    Controller();
+    Controller(ControllerId i_id);
 
     void bindObject(IControlable* i_object);
     void unbindObject();
 
+    int getId() const { return d_id; }
+
+
+    virtual bool onKeyPressed(Key i_key) override;
+    virtual bool onKeyDown(Key i_key) override;
+    virtual bool onKeyUp(Key i_key) override;
+
   private:
+
+    ControllerId d_id;
 
     IControlable* d_bindedObject;
 

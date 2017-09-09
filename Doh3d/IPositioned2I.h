2b1ff237b025f4a12b63a86165013bf588850836
Anton Egorov <dohxehapo@gmail.com>
2017-09-10 01:12:14 +0300

    (~) Refactored IPositioned2I

--- Doh3d/IPositioned2I.h
+++ Doh3d/IPositioned2I.h
@@ -6,23 +6,19 @@
 namespace Doh3d
 {
 
-  class IPositioned2I
-  {
-  public:
+class IPositioned2I
+{
+public:
 
-    virtual ~IPositioned2I() { }
+  virtual ~IPositioned2I() { }
 
-    virtual const Position2I getPosition() const { return d_position; }
-    virtual void setPosition(const Position2I& pPosition) { d_position = pPosition; }
+  virtual const Position2I getPosition() const { return d_position; }
+  virtual void setPosition(const Position2I& pPosition) { d_position = pPosition; }
 
-  protected:
+protected:
 
-    IPositioned2I() : d_position(0, 0) { }
+  Position2I d_position;
 
-  private:
-
-    Position2I d_position;
-
-  };
+};
 
 } // ns Doh3d

5b445a7a2252bf5918800c13ea277f083acb0917
Anton Egorov <dohxehapo@gmail.com>
2017-09-10 01:13:01 +0300

    (+) Added Position2F

---
+++ Doh3d/IPositioned2F.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Position2F.h"
+
+
+namespace Doh3d
+{
+
+class IPositioned2F
+{
+public:
+
+  virtual ~IPositioned2F() { }
+
+  virtual const Position2F getPosition() const { return d_position; }
+  virtual void setPosition(const Position2F& pPosition) { d_position = pPosition; }
+
+protected:
+
+  Position2F d_position;
+
+};
+
+} // ns Doh3d
---
+++ Doh3d/Position2F.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "Vector2F.h"
+
+
+namespace Doh3d
+{
+
+using Position2F = Vector2F;
+
+} // ns Doh3d

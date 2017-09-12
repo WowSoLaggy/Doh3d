81c0ee8d733546552ff2622fb4a493e6f628154a
Anton Egorov <dohxehapo@gmail.com>
2017-09-12 23:35:11 +0300

    (*) Changed an order of the phase vector update

--- Doh3d/IMovable2F.cpp
+++ Doh3d/IMovable2F.cpp
@@ -10,8 +10,8 @@ namespace Doh3d
     if (!beforeUpdate(i_dt))
       return false;
 
-    d_speed += d_accel * i_dt;
     d_position += d_speed * i_dt;
+    d_speed += d_accel * i_dt;
 
     if (!afterUpdate(i_dt))
       return false;
